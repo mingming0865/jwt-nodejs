97c04a40ff856e5430793ea1d9bf2456f2cffca8
mingming0865 <mingming0865@gmail.com>
2025-01-10 15:00:48 +0700

    mvc

---
+++ src/controller/homeController.js
@@ -0,0 +1,13 @@
+const handleHelloWorld = (req, res) => {
+    return res.render("home.ejs");
+}
+
+const handleUserPage = (req, res) => {
+    //model => get data from database
+
+    return res.render("user.ejs");
+}
+
+module.exports = {
+    handleHelloWorld, handleUserPage
+}
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,4 +1,5 @@
 import express from "express";
+import homeController from '../controller/homeController'
 
 const router = express.Router();
 
@@ -6,10 +7,11 @@ const router = express.Router();
  * 
  * @param {*} app : express app
  */
+
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world");
-    })
+    //path, handler
+    router.get("/", homeController.handleHelloWorld);
+    router.get("/user", homeController.handleUserPage);
 
     return app.use("/", router);
 }
